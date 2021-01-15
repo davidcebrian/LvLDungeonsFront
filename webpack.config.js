f48a7283ea2abcac6f98989cd4a3f67f64aab9d8
David <davidcebriancortes0@gmail.com>
2021-01-15 09:03:52 +0100

    tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         loader: 'postcss-loader',
         options: {
           postcssOptions: {
