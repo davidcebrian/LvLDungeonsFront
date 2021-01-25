9edb4354eb671022b82d44cfcebc09f50f8390b1
David <davidcebriancortes0@gmail.com>
2021-01-25 11:54:51 +0100

    registro completo

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -16,32 +16,34 @@ export class RegistroComponent implements OnInit {
 
 
   constructor(private build: FormBuilder, private userService: DatabaseService, private router: Router) {
-    this.registerForm = this.build.group({
-      nombre: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      username: ['', Validators.required, Validators.minLength(3)],
-      password: ['', Validators.required,  Validators.minLength(3)],
-      repetir_password: ['', [Validators.required, this.compararPasswords]],
-      edad: ['', Validators.required],
-    })
   }
 
   ngOnInit(): void {
+    this.registerForm = this.build.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required,  Validators.minLength(3)]],
+      repetir_password: ['', [Validators.required, this.compararPasswords]],
+      edad: ['', Validators.required],
+    }, {validator: this.compararPasswords('password', 'repetir_password')})
   }
 
-  private compararPasswords(control:AbstractControl){
-    const password = control.get('password');
-    const repetir_password = control.get('repetir_password');
+  private compararPasswords(control: string, control2: string){
+    return(formGroup: FormGroup) =>{
+      const password = formGroup.controls[control];
+      const repetir_password = formGroup.controls[control2];
+      
+      if(!password || !repetir_password) return null
 
-    let error = null;
+      if(repetir_password.errors && !repetir_password.errors.passwordMismatch) return null
 
-
-    //si no coinciden return el error
-    if (password != repetir_password) {
-        error = {...error, error:'Las contraseñas no coinciden'}
+      if(password.value !== repetir_password.value){
+        repetir_password.setErrors({passwordMismatch: true});
+      } else {
+        repetir_password.setErrors(null);
+      }
     }
-
-    return error;
   }
 
   registro(): void{
